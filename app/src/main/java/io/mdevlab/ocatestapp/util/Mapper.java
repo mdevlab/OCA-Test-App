a40187d71163088b110ece451ac727394c2f9535
Husayn Hakeem <husaynhakeem@gmail.com>
2017-04-26 17:09:27 +0100

    util package reviewed

--- app/src/main/java/io/mdevlab/ocatestapp/util/Mapper.java
+++ app/src/main/java/io/mdevlab/ocatestapp/util/Mapper.java
@@ -12,16 +12,30 @@ import io.realm.RealmModel;
  * Created by husaynhakeem on 4/21/17.
  */
 
-public class Mapper <E extends RealmModel> {
+public class Mapper<E extends RealmModel> {
 
+    // Unique instance of this class
     private static Mapper mapper;
 
+    /**
+     * Method that returns the unique instance of this class
+     * Also instantiates it if needed
+     *
+     * @return
+     */
     public static Mapper instance() {
         if (mapper == null)
             mapper = new Mapper();
         return mapper;
     }
 
+    /**
+     * Generic method that transforms and json input from a file to data in a realm table
+     *
+     * @param context  Context of component calling this method
+     * @param fileName Name of file containing the json data
+     * @param clazz    Name of the RealmModel class the json input will be put in
+     */
     public void fromJsonToRealm(final Context context, final String fileName, final Class<E> clazz) {
         Realm.getDefaultInstance()
                 .executeTransaction(new Realm.Transaction() {
