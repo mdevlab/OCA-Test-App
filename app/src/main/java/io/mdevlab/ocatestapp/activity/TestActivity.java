f370b1d3559838e3e175faa18ca641dae27e1c01
bachiri Taoufiq Abderrahman <bachiri.abderrahman@gmail.com>
2017-05-04 23:51:53 +0100

    add questions activity

--- app/src/main/java/io/mdevlab/ocatestapp/activity/TestActivity.java
+++ app/src/main/java/io/mdevlab/ocatestapp/activity/TestActivity.java
@@ -2,29 +2,45 @@ package io.mdevlab.ocatestapp.activity;
 
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
-import io.mdevlab.ocatestapp.fragment.QuestionFragment;
+import java.util.List;
+
 import io.mdevlab.ocatestapp.R;
 import io.mdevlab.ocatestapp.model.TestQuestion;
 
-import static io.mdevlab.ocatestapp.test.TestQuestionTest.createRandomTestQuestion;
-
 public class TestActivity extends AppCompatActivity {
+
+    private ViewPager mTestQuestionViewPager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
 
-        TestQuestion question = createRandomTestQuestion(0,TestActivity.this);
+        mTestQuestionViewPager = (ViewPager) findViewById(R.id.test_questions_viewpager);
 
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction ft = fm.beginTransaction();
-        ft.add(R.id.question_container, QuestionFragment.newInstance(question,false));
+        List<TestQuestion> listQuestion =
 
-        ft.commit();
+
+    }
+
+
+
+
+    public void nextFragement() {
+        //        TestQuestion question = createRandomTestQuestion(0,TestActivity.this);
+//
+//        FragmentManager fm = getSupportFragmentManager();
+//        FragmentTransaction ft = fm.beginTransaction();
+//        ft.add(R.id.question_container, QuestionFragment.newInstance(question,false));
+//
+//        ft.commit();
+    }
+
+    public void PreviousFragement() {
+
     }
 }
---
+++ app/src/main/java/io/mdevlab/ocatestapp/adapter/TestQuestionAdapter.java
@@ -0,0 +1,38 @@
+package io.mdevlab.ocatestapp.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.List;
+
+import io.mdevlab.ocatestapp.fragment.QuestionFragment;
+import io.mdevlab.ocatestapp.model.TestQuestion;
+
+/**
+ * Created by bachiri on 5/4/17.
+ */
+
+public class TestQuestionAdapter extends FragmentStatePagerAdapter {
+
+    List<TestQuestion> mTestQuestionList;
+
+    public TestQuestionAdapter(FragmentManager fm, List<TestQuestion> testQuestionList) {
+        super(fm);
+        this.mTestQuestionList = testQuestionList;
+    }
+
+    @Override
+    public int getCount() {
+        return mTestQuestionList.size();
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return QuestionFragment.newInstance(mTestQuestionList.get(position),false);
+    }
+}
+
+
+
+
