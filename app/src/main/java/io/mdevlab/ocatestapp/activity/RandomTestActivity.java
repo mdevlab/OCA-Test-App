c743edb9ad9016a30e8a7c088297df5e044ae48e
bachiri Taoufiq Abderrahman <bachiri.abderrahman@gmail.com>
2017-05-08 22:10:03 +0100

    add test Activity

---
+++ app/src/main/java/io/mdevlab/ocatestapp/activity/RandomTestActivity.java
@@ -0,0 +1,15 @@
+package io.mdevlab.ocatestapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import io.mdevlab.ocatestapp.R;
+
+public class RandomTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_random_test);
+    }
+}
